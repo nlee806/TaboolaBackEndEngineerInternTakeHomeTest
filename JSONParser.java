9a2605d5a121745dd0e26b0680c50ecb5998401c
nlee806 <nlee806@gmail.com>
2023-11-29 19:59:59 -0800

    Added example input/output specifications from requirements to main(). Initialized parse() class, initialized new output HashMap.

--- JSONParser.java
+++ JSONParser.java
@@ -5,11 +5,36 @@
 
 
 **/
+import java.util.*;
+
 
 public class JSONParser{
 	
-	public static void main(String[] args){
+	/**
+		Parses json String and returns Map<String,Object>
+	**/
+	public static Map<String,Object> parse(String json){
+		Map<String,Object> outputMap = new HashMap<String,Object>();
+		 
 		
+		
+		
+		return outputMap;
+	}
+	
+	/**
+		Main driver with example. Replace exampleString with test strings.
+	**/
+	public static void main(String[] args){
+		String exampleString = "{\"debug\" : \"on\",\"window\" : {\"title\" : \"sample\",\"size\": 500}}";
+		//parse() is static already but to keep in line with the example input:
+		Map<String,Object> output = JSONParser.parse(exampleString);
+		/*
+		assert output.get("debug").equals("on");
+		//XXX weirdly written
+		assert (Map<String, Object>(output.get("window")).get("title")).equals("sample");
+		assert (Map<String, Object>(output.get("window")).get("size")).equals(500);
+		*/
 	}
 	
 }
