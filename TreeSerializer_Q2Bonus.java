f7697c45cd696d2b2d996f94966f800dc9bcce85
nlee806 <nlee806@gmail.com>
2023-12-02 11:36:27 -0800

    Initialized TreeSerializer classes.

--- TreeSerializer_Q2Bonus.java
+++ TreeSerializer_Q2Bonus.java
@@ -8,6 +8,17 @@
 
 public class TreeSerializer_Q2Bonus{
 	
+	public class Node{
+		Node left;
+		Node right;
+		int num;
+	}
+	
+	public interface TreeSerializer{
+		String serialize(Node root);
+		Node deserialize(String str);
+	}
+		
 	public static void main(String[] args){
 		
 	}
